ad9fb2eab43654ef5a162827c2b5c98d4231f922
Vignesh <vigneskdr777@gmail.com.com>
2024-12-02 14:40:10 +0530

    bug fixed in the wallet

--- controller/user/wallet.js
+++ controller/user/wallet.js
@@ -3,25 +3,28 @@ const Wallet = require('../../models/wallet')
 
 const LoadWallet = async (req,res)=>{
     try {
-        // const currentPage = parseInt(req.query.page)||2;
-        // const itemsPerPage = 5;
         const userEmail = req.session.user?.email;
-        const user = await User.findOne({email:userEmail})
-        // const totalAmount = await Wallet.countDocuments({userId:user._id})
-
-            // const totalPages = Math.ceil(totalAmount/itemsPerPage)
-            // const skip = (currentPage-1)*itemsPerPage
-        const wallet = await Wallet.findOne({userId:user._id}).sort({createdAt:-1})
-
         if(!userEmail){
            return res.redirect('/login')
+        }
+        
+        const user = await User.findOne({email:userEmail})
+        let wallet = await Wallet.findOne({userId:user._id}).sort({createdAt:-1})
+        
+        // Create wallet if it doesn't exist
+        if (!wallet) {
+            wallet = new Wallet({
+                userId: user._id,
+                balance: 0,
+                transaction: []
+            });
+            await wallet.save();
         }
 
         res.render('wallet', {wallet, user})
 
     } catch (error) {
         console.log(error, 'cant get this page')
-
         return res.status(500).send('server error!')
     }
 }
